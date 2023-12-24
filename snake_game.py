63d8f2d5aea55940a46caab3e782181a345fcbd2
Max <maxrijkers@outlook.com>
2023-12-24 17:39:01 +0100

    added optimizer and replay memory

--- snake_game.py
+++ snake_game.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 import numpy as np
 import math
-from collections import namedtuple
+from collections import namedtuple, deque
 
 
 pygame.init()
@@ -48,6 +48,21 @@ Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return np.random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
@@ -85,6 +100,7 @@ class SNAKE:
             self.optimizer = optim.AdamW(
                 self.policy_net.parameters(), lr=LR, amsgrad=True
             )
+            self.memory = ReplayMemory(10000)
 
         self.speed = int(size_x / 10)
         self.length = start_length
@@ -186,6 +202,7 @@ class MAP:
         self.snake = SNAKE(self.x_blocks, self.y_blocks, self.autonomous)
         self.last_direction = "right"
         self.direction = "right"
+        self.steps_done = 0
 
     def create_map(self):
         for i in range(self.block_size):
@@ -339,7 +356,10 @@ class MAP:
         direction_index = np.argmax(action)
         self.direction = ACTION_OPTIONS[direction_index]
 
-    def run_autonomous_game_loop(self, action):
+    def run_autonomous_game_loop(self, action=None):
+        if action == None:
+            state = self.get_state()
+            action = self.select_action(state)
         while True:
             self.AI_control(action)
 
@@ -362,23 +382,70 @@ class MAP:
             clock.tick(60)
             yield (self.get_state(), 0, False, False)
 
+    def select_action(self, state):
+        sample = np.random.random()
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
+            -1.0 * self.steps_done / EPS_DECAY
+        )
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # Will return a list of sigmoid values
+                return self.policy_net(state)
+        else:
+            # Return random values to explore new possibilities
+            return np.array(np.random.random_sample(4))
 
-steps_done = 0
+    def optimize_model(self):
+        if len(self.memory) < BATCH_SIZE:
+            return
+        transitions = self.memory.sample(BATCH_SIZE)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(
+            tuple(map(lambda s: s is not None, batch.next_state)),
+            device=device,
+            dtype=torch.bool,
+        )
+        non_final_next_states = torch.cat(
+            [s for s in batch.next_state if s is not None]
+        )
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
 
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
-def select_action(state, policy_net):
-    sample = np.random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
-        -1.0 * steps_done / EPS_DECAY
-    )
-    steps_done += 1
-    if sample > eps_threshold:
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1).values
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(BATCH_SIZE, device=device)
         with torch.no_grad():
-            # Will return a list of sigmoid values
-            return policy_net(state)
-    else:
-        # Return random values to explore new possibilities
-        return np.array(np.random.random_sample(4))
+            next_state_values[non_final_mask] = (
+                self.target_net(non_final_next_states).max(1).values
+            )
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
 
 
 autonomous = input("Autonomous: ")
@@ -392,7 +459,7 @@ else:
         state = snake_map.run_autonomous_game_loop()
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
         while True:
-            action = select_action(state)
+            action = snake_map.select_action(state)
             (
                 observation,
                 reward,
@@ -410,13 +477,13 @@ else:
                 ).unsqueeze(0)
 
             # Create a transition
-            step_data = Transition(state, action, next_state, reward)
+            snake_map.memory.push(state, action, next_state, reward)
 
             # Move to the next state
             state = next_state
 
             # Perform one step of the optimization (on the policy network)
-            optimize_model(step_data)
+            snake_map.optimize_model()
 
             # Soft update of the target network's weights
             # θ′ ← τ θ + (1 −τ )θ′
